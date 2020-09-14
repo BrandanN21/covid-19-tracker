f0974746941faf33ab30c9c65cc98fb5e8fef9e5
Brandan <blnaef99@gmail.com>
2020-09-14 17:11:06 -0500

    start setting up app structure

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="app">
       <h1>lets build a covid 19 tracker</h1>
+      {/* header */}
+      {/* Title + select input dropdown field */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
