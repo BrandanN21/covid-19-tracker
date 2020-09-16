820ea3eae7705faeb95596e23199f44855f6ebb7
Brandan <blnaef99@gmail.com>
2020-09-15 23:51:49 -0500

    added header and title

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([]); //set initial value = empty array
+  const [country, setCountry] = useState("WorldWide");
+  //state = how to write a variable in REACT <<<<<
+
+  //https://disease.sh/v3/covid-19/countries
+
+  //useeffect = runs a piece of code
+  //based on a given condition
+
+  useEffect(() => {
+    //runs when the component loads and not again
+    //async -> send request to server, wait for it, do something with info
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []); //add in [] it will run everytime the [] changes
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
-      <h1>lets build a covid 19 tracker</h1>
-      {/* header */}
-      {/* Title + select input dropdown field */}
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            {/*loop thorugh the countries and show dropdown list of options */}
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
       {/* Info Boxes */}
       {/* Info Boxes */}
       {/* Info Boxes */}
